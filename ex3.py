3560aae1921c3b77188bf124215f767db59c541d
Grant Moe <grant.edward.moe@gmail.com>
2019-02-24 21:07:27 -0800

    Exercise 3

---
+++ ex3.py
@@ -0,0 +1,41 @@
+# Prints "I will now count my chickens:"
+print("I will now count my chickens:")
+
+# Prints "Hens" then prints the result of twenty-five 
+# added to the result of thirty divded by six: thirty
+print("Hens", 25.0 + 30.0 / 6.0)
+# Prints "Roosters" then prints the result of 
+# One-hundred minus the result of twenty-five times three (which is seventy-five)
+# modulo four (which is three)": ninety-seven
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+# Prints "Now I will count the eggs:"
+print("Now I will count the eggs:")
+
+# Prints the result of three plus two plus one minus 5 plus the result of four modulo two,
+# (which is zero) minus the result of one divded by four, which is twenty-five hundredths,
+# plus six: six and seventy-five hundredths
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# Prints "Is it true that 3 + 2 < 5 - 7?"
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Prints "False"
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+# Prints "What is 3 + 2? then "5"
+print("What is 3 + 2?", 3.0 + 2.0)
+# Prints "What is 5 - 7, then "-2"
+print("What is 5 - 7?", 5.0 - 7.0)
+
+# Prints "Oh, that's why it's False."
+print("Oh, that's why it's False.")
+# Prints "How bout some more."
+print("How about some more.")
+
+# Prints "Is it greater? then prints "True", the result of comparison 5 > -2
+print("Is it greater?", 5.0 > -2.0)
+# Prints "Is it greater or equal?" then prints "True", the result of the comparison 5 >= -2
+print("Is it greater or equal?", 5.0 >= -2.0)
+# Prints "Is it less or equal?" then prints "False", whcih is the result of 5 <= -2
+print("Is it less or equal?", 5.0 <= -2.0)
